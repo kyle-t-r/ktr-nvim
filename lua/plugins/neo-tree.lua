3ebc762786f43067105f0fe57666bd5a70bd15c4
ktr <kyletradick@gmail.com>
2025-06-21 12:57:02 -0400

    Initial commit

---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,21 @@
+return {
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons",
+      "MunifTanjim/nui.nvim",
+    },
+    lazy = false,
+      ---@module "neo-tree"
+---@diagnostic disable-next-line: undefined-doc-name
+      ---@type neotree.Config?
+    opts = {
+    },
+    config = function()
+      vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>', {})
+    end
+  },
+
+}
